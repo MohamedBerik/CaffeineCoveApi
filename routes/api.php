5d131a5b15cafa887d673afd1ea567cdb7701d7c
MohamedBerik <monsieur.atef@gmail.com>
2026-01-21 17:00:28 +0200

    Fix CSRF / update middleware

--- routes/api.php
+++ routes/api.php
@@ -108,17 +108,17 @@ Route::middleware([
 // });
 
 
-// Route::get('/db-test', function () {
-//     try {
-//         DB::connection()->getPdo();
-//         return response()->json([
-//             'status' => 'Database connection OK',
-//             'database' => env('DB_DATABASE')
-//         ]);
-//     } catch (\Exception $e) {
-//         return response()->json([
-//             'status' => 'Database connection FAILED',
-//             'error' => $e->getMessage()
-//         ], 500);
-//     }
-// });
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+        return response()->json([
+            'status' => 'Database connection OK',
+            'database' => env('DB_DATABASE')
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'Database connection FAILED',
+            'error' => $e->getMessage()
+        ], 500);
+    }
+});
