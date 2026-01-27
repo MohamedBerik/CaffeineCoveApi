66c25095ac94acbdb646ec6b1cae7f625b4ce8f5
MohamedBerik <monsieur.atef@gmail.com>
2026-01-27 14:48:55 +0200

    fix files

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://caffeine-cove-cafe.vercel.app', 'https://caffeinecoveapi-production-a107.up.railway.app'],
+    'allowed_origins' => ['https://caffeine-cove-cafe.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
@@ -16,6 +16,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
