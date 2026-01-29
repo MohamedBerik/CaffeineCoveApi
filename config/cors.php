bb35f05060cf5c8a46155e26e56bf69116de3f85
MohamedBerik <monsieur.atef@gmail.com>
2026-01-29 18:04:27 +0200

    fix files

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+    'paths' => ['api/*', 'admin/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
 
     'allowed_methods' => ['*'],
 
