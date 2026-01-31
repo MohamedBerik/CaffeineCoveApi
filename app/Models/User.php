171f80f3b239cc0952274158bc33f77a942049ff
MohamedBerik <monsieur.atef@gmail.com>
2026-01-31 16:08:10 +0200

    fix files

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,25 +5,15 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, HasRoles;
 
     protected $fillable = ['name', 'email', 'password', 'role', 'status'];
 
     protected $hidden = ['password', 'remember_token'];
 
     protected $casts = ['email_verified_at' => 'datetime'];
-
-    // ← العلاقة
-    public function roles()
-    {
-        return $this->belongsToMany(
-            \App\Models\Role::class, // نموذج Role
-            'role_user',             // جدول pivot
-            'user_id',               // مفتاح المستخدم
-            'role_id'                // مفتاح الدور
-        );
-    }
 }
