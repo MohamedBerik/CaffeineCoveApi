8a81856f6260d4c58daecd101e6e095bdba9b1ba
MohamedBerik <monsieur.atef@gmail.com>
2026-02-21 10:40:30 +0200

    fix

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -16,15 +16,15 @@ class ProductResource extends JsonResource
     {
         return [
             "id" => $this->id,
-            "image"=>$this->product_image,
+            "image" => $this->product_image,
             'image_url' => $this->product_image ? url('img/product/' . $this->product_image) : null,
-            "title"=>$this->title_en,
-            "title_ar"=>$this->title_ar,
-            "description"=>$this->description_en,
-            "description_ar"=>$this->description_ar,
-            "price"=>$this->price,
-            "quantity"=>$this->quantity,
-            "category_id"=>$this->category_id,
+            "title" => $this->title_en,
+            "title_ar" => $this->title_ar,
+            "description" => $this->description_en,
+            "description_ar" => $this->description_ar,
+            "unit_price" => $this->unit_price,
+            "quantity" => $this->quantity,
+            "category_id" => $this->category_id,
         ];
     }
 }
