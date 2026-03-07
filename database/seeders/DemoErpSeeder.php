fe5a670c3b8c4c0f78a222de9a1a65aa0ae67f5d
MohamedBerik <monsieur.atef@gmail.com>
2026-03-07 11:24:24 +0200

    fix

--- database/seeders/DemoErpSeeder.php
+++ database/seeders/DemoErpSeeder.php
@@ -63,7 +63,7 @@ class DemoErpSeeder extends Seeder
                     'password' => Hash::make('12345678'),
                     'company_id' => $company->id,
                     'role' => 'admin',
-                    'status' => '1',
+                    'status' => 1,
                     'is_super_admin' => 0,
                 ]
             );
