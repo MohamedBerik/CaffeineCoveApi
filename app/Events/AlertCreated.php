5b01b68a68306e84d7dee20bad1ffe979c754905
MohamedBerik <monsieur.atef@gmail.com>
2026-04-06 23:01:43 +0200

    fix

--- app/Events/AlertCreated.php
+++ app/Events/AlertCreated.php
@@ -4,9 +4,13 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class AlertCreated implements ShouldBroadcast
 {
+    use Dispatchable, SerializesModels;
+
     public $alert;
 
     public function __construct($alert)
