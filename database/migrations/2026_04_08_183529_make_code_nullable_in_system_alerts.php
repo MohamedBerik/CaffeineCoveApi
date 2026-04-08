185fb15dccabbd737552b24c9c9306bdeebaca3a
MohamedBerik <monsieur.atef@gmail.com>
2026-04-08 20:07:12 +0200

    fix

---
+++ database/migrations/2026_04_08_183529_make_code_nullable_in_system_alerts.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('system_alerts', function (Blueprint $table) {
+            $table->string('code')->nullable()->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('system_alerts', function (Blueprint $table) {
+            $table->string('code')->nullable(false)->change();
+        });
+    }
+};
