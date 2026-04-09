0a33f9f0529dbde37e8f1135fbea16c689ba1dec
MohamedBerik <monsieur.atef@gmail.com>
2026-04-09 13:12:00 +0200

    fix

--- routes/channels.php
+++ routes/channels.php
@@ -8,15 +8,17 @@ use Illuminate\Support\Facades\Broadcast;
 |--------------------------------------------------------------------------
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
+// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });
 
+// Broadcast::channel('company.{companyId}', function ($user, $companyId) {
+//     return (int) $user->company_id === (int) $companyId;
+// });
 
 Broadcast::channel('company.{companyId}', function ($user, $companyId) {
-    return (int) $user->company_id === (int) $companyId;
+    return true; // ✅ للتجربة فقط
 });
-
 
 // Broadcast::channel('alerts', function ($user) {
 //     return $user != null;
