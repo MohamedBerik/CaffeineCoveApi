8f61762553350ff66ef2996f96a586f6ce42d68f
MohamedBerik <monsieur.atef@gmail.com>
2026-04-09 12:58:36 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,7 @@ use Illuminate\Support\Facades\Broadcast;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
-Broadcast::routes(['middleware' => ['auth:sanctum']]);
+// Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
 /*
 |--------------------------------------------------------------------------
