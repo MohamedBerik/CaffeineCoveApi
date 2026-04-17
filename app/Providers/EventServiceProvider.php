6c8600fe01756e7a5b58427d5911dd60b33e4e21
MohamedBerik <monsieur.atef@gmail.com>
2026-04-17 17:05:53 +0200

    fix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -8,30 +8,9 @@ use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvi
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
+    // app/Providers/EventServiceProvider.php
+
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
+        // ✅ علّق كل الـ Listeners مؤقتًا
     ];
-
-    /**
-     * Register any events for your application.
-     */
-    public function boot(): void
-    {
-        parent::boot();
-    }
-
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     */
-    public function shouldDiscoverEvents(): bool
-    {
-        return false;
-    }
 }
