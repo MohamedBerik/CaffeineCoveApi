0b03f703ee21c95a2ef0bc8a80da0e0a6ffb6029
MohamedBerik <monsieur.atef@gmail.com>
2026-04-17 14:53:29 +0200

    Fix CORS and Sanctum configuration

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://caffeine-cove-cafe.vercel.app'],
+    'allowed_origins' => [
+        'https://caffeine-cove-cafe.vercel.app',
+        'https://caffeinecoveapi-production-a107.up.railway.app',
+        'http://localhost:3000',
+        'http://localhost:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
