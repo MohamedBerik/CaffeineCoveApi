9b8a3bbe03b356af23e1471e117d1801fcb545ee
MohamedBerik <monsieur.atef@gmail.com>
2026-04-18 19:58:25 +0200

    fix

--- app/Http/Middleware/SetTenant.php
+++ app/Http/Middleware/SetTenant.php
@@ -6,6 +6,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use App\Services\Tenant;
+use Illuminate\Support\Facades\Log;
 
 class SetTenant
 {
@@ -13,25 +14,11 @@ class SetTenant
     public function handle(Request $request, Closure $next)
     {
         $user = $request->user();
+        Log::info('USER IN TENANT', ['user' => $user]);
 
-        // if ($user) {
-        //     if ($user->is_super_admin) {
-        //         Tenant::setIsSuperAdmin(true);
-        //         // ✅ لو Super Admin داخل شركة، نستخدم company_id بتاعه
-        //         if ($user->company_id) {
-        //             Tenant::setId($user->company_id);
-        //         } else {
-        //             Tenant::setId(null);
-        //         }
-        //     } elseif ($user->company_id) {
-        //         Tenant::setId($user->company_id);
-        //         Tenant::setIsSuperAdmin(false);
-        //     } else {
-        //         Tenant::setId(null);
-        //         Tenant::setIsSuperAdmin(false);
-        //     }
-        // }
-
+        if (!$user) {
+            return $next($request);
+        }
 
         if ($user->is_super_admin) {
             Tenant::setIsSuperAdmin(true);
@@ -39,9 +26,13 @@ class SetTenant
             if ($user->company_id) {
                 Tenant::setId($user->company_id);
             } else {
-                Tenant::setId(1); // 👈 TEMP FIX
+                Tenant::setId(null); // ❗ مش 1
             }
+        } else {
+            Tenant::setId($user->company_id);
+            Tenant::setIsSuperAdmin(false);
         }
+
         return $next($request);
     }
 }
