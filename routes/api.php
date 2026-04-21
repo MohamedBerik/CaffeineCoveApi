8def18e94012d96edc11d13660d079524975315a
MohamedBerik <monsieur.atef@gmail.com>
2026-04-21 17:41:35 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -107,7 +107,7 @@ Route::middleware(['auth:sanctum', 'company.user'])->group(function () {
 |--------------------------------------------------------------------------
 */
 Route::prefix('erp')
-    ->middleware([\App\Http\Middleware\SetTenant::class, 'auth:sanctum', 'company.user'])
+    ->middleware(['auth:sanctum', 'company.user'])
     ->group(function () {
 
         // ==================== DASHBOARD & REPORTS ====================
