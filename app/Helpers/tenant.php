84929b161d5088cee39d03409944171f409d7b0b
MohamedBerik <monsieur.atef@gmail.com>
2026-04-21 17:12:23 +0200

    fix

--- app/Helpers/tenant.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!function_exists('tenant_cache_key')) {
-    function tenant_cache_key(string $key): string
-    {
-        $tenantId = \App\Services\Tenant::id() ?? 'global';
-        return "tenant_{$tenantId}_{$key}";
-    }
-}
