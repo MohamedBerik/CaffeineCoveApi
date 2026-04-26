7dc373eb18a08e98c1af703461ec1224c5309995
MohamedBerik <monsieur.atef@gmail.com>
2026-04-26 22:24:20 +0300

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\SecurityHeaders::class,
             \App\Http\Middleware\ValidateFileUpload::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\SetTenant::class,
+            // \App\Http\Middleware\SetTenant::class,
         ],
     ];
 
