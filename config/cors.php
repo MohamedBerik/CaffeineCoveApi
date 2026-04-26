d4e2e9a145ee4313635a3daf2ea8f8614f4c2dfb
MohamedBerik <monsieur.atef@gmail.com>
2026-04-26 20:45:27 +0300

    fix

--- config/cors.php
+++ config/cors.php
@@ -1,40 +1,40 @@
 <?php
 
-return [
-    'paths' => ['api/*'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'https://caffeine-cove-cafe.vercel.app',
-    ],
-
-    'allowed_headers' => ['*'],
-
-    'supports_credentials' => true,
-];
-
 // return [
-
-//     'paths' => ['api/*', 'admin/*', 'sanctum/csrf-cookie', 'broadcasting/auth', 'login', 'logout', 'register'],
+//     'paths' => ['api/*'],
 
 //     'allowed_methods' => ['*'],
 
 //     'allowed_origins' => [
 //         'https://caffeine-cove-cafe.vercel.app',
-//         'https://caffeinecoveapi-production-a107.up.railway.app',
-//         'http://localhost:3000',
-//         'http://localhost:5173',
 //     ],
-
-//     'allowed_origins_patterns' => [],
 
 //     'allowed_headers' => ['*'],
 
-//     'exposed_headers' => [],
-
-//     'max_age' => 0,
-
 //     'supports_credentials' => true,
-
 // ];
+
+return [
+
+    'paths' => ['api/*', 'admin/*', 'sanctum/csrf-cookie', 'broadcasting/auth', 'login', 'logout', 'register'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://caffeine-cove-cafe.vercel.app',
+        'https://caffeinecoveapi-production-a107.up.railway.app',
+        'http://localhost:3000',
+        'http://localhost:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
