fcd6acd76caef71d88fe046102c6c35090b3e1e9
MohamedBerik <monsieur.atef@gmail.com>
2026-04-26 13:51:57 +0300

    fix

--- app/Listeners/AuditListeners.php
+++ app/Listeners/AuditListeners.php
@@ -13,12 +13,28 @@ class LogFailedLogin
     public function handle(FailedLogin $event)
     {
         try {
-            ActivityLog::create([]);
+            ActivityLog::create([
+                'company_id' => null,
+                'user_id' => null,
+                'action' => 'auth.failed_login',
+                'subject_type' => 'User',
+                'subject_id' => null,
+                'properties' => [
+                    'email' => $event->email,
+                    'ip' => $event->ip,
+                    'reason' => $event->reason,
+                    'attempted_at' => now(),
+                ],
+            ]);
         } catch (\Exception $e) {
             Log::error('Cannot log failed login: ' . $e->getMessage());
         }
 
-        Log::warning('Failed login attempt', []);
+        Log::warning('Failed login attempt', [
+            'email' => $event->email,
+            'ip' => $event->ip,
+            'reason' => $event->reason,
+        ]);
     }
 }
 
