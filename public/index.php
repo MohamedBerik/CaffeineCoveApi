ddca2d7b6ef4e03c69b2fc59cd5b97be5c045291
MohamedBerik <monsieur.atef@gmail.com>
2026-04-27 09:13:42 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -1,19 +1,19 @@
 <?php
 
 //للاختبار فقط :
-// if (isset($_SERVER['HTTP_ORIGIN'])) {
-//     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-//     header('Access-Control-Allow-Credentials: true');
-//     header('Access-Control-Max-Age: 86400');    // cache for 1 day
-// }
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
 
-// if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-//     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-//         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-//     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-//         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-//     exit(0);
-// }
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    exit(0);
+}
 //.............................................................
 
 use Illuminate\Contracts\Http\Kernel;
