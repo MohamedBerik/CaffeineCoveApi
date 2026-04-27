b222409a7989be2dcb93447a64f2e7e852a48a65
MohamedBerik <monsieur.atef@gmail.com>
2026-04-27 19:03:30 +0300

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,8 +8,8 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class,              // ✅ الأول
-        \App\Http\Middleware\TrustProxies::class,       // ✅ التاني
+        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
