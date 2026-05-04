ff7b3923e45f9a72e02ecbe735353e776232b2b1
MohamedBerik <monsieur.atef@gmail.com>
2026-05-04 18:41:21 +0300

    fix

--- app/Http/Controllers/API/Erp/CustomerController.php
+++ app/Http/Controllers/API/Erp/CustomerController.php
@@ -14,7 +14,7 @@ class CustomerController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(Customer::class, 'customer', ['except' => ['index', 'store']]);
+        $this->authorizeResource(Customer::class, 'customer', ['except' => ['index', 'store', 'update']]);
     }
 
     public function index(Request $request)
