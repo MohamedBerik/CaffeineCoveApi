0484e09e3430cd35eff2f0e9ae2a484c6deb8a97
MohamedBerik <monsieur.atef@gmail.com>
2026-05-04 17:15:30 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -63,7 +63,8 @@ use Illuminate\Support\Facades\DB;
 |--------------------------------------------------------------------------
 */
 
-Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:login');
+// Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:login');
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:register');
 
 /*
