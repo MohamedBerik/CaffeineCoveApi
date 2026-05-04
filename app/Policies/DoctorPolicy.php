ba93090cc8fe9e7c46e62c43e8e5598c621439a8
MohamedBerik <monsieur.atef@gmail.com>
2026-05-05 00:19:46 +0300

    fix

--- app/Policies/DoctorPolicy.php
+++ app/Policies/DoctorPolicy.php
@@ -18,9 +18,22 @@ class DoctorPolicy
 
     public function view(User $user, Doctor $doctor): bool
     {
-        return $user->hasPermissionTo('doctors.view')
-            && $this->hasCompanyAccess($user, $doctor->company_id)
-            && $this->hasBranchAccess($user, $doctor->branch_id);
+        $hasPerm = $user->hasPermissionTo('doctors.view');
+        $companyOk = $this->hasCompanyAccess($user, $doctor->company_id);
+        $branchOk = $this->hasBranchAccess($user, $doctor->branch_id);
+
+        \Log::info('DoctorPolicy view check', [
+            'user_id' => $user->id,
+            'user_branch' => $user->branch_id,
+            'doctor_id' => $doctor->id,
+            'doctor_branch' => $doctor->branch_id,
+            'dr_company' => $doctor->company_id,
+            'hasPerm' => $hasPerm,
+            'companyOk' => $companyOk,
+            'branchOk' => $branchOk,
+        ]);
+
+        return $hasPerm && $companyOk && $branchOk;
     }
 
     public function create(User $user): bool
