40eea0e0c1bb3c434d3af1830d110f83c5e9d01f
MohamedBerik <monsieur.atef@gmail.com>
2026-05-09 12:21:11 +0300

    fix

---
+++ database/migrations/2026_05_09_121534_add_user_id_to_employees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Schema::table('employees', function ($table) {
+            $table->unsignedBigInteger('user_id')->nullable()->after('branch_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            //
+        });
+    }
+}
