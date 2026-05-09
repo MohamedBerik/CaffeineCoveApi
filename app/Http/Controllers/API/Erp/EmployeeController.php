5eafd7576472af1c614ce91026d292b3dc79b90d
MohamedBerik <monsieur.atef@gmail.com>
2026-05-09 13:23:02 +0300

    fix

--- app/Http/Controllers/API/Erp/EmployeeController.php
+++ app/Http/Controllers/API/Erp/EmployeeController.php
@@ -198,7 +198,7 @@ class EmployeeController extends Controller
         return response()->json([
             "msg"    => "Updated Successfully",
             "status" => 200,
-            "data"   => $employee->fresh()->load('user')
+            "data"   => new EmployeeResource($employee->fresh()->load('user'))
         ]);
     }
 }
