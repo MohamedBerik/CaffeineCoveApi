5abb6f01c69b520f380d9aaa0913f14c48c03018
MohamedBerik <monsieur.atef@gmail.com>
2026-05-10 09:08:56 +0300

    fix

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -90,6 +90,8 @@ class Appointment extends Model
         'is_upcoming',
         'is_past',
         'can_be_modified',
+        'status_label',
+        'status_color',
     ];
 
     // ============ Relationships ============
