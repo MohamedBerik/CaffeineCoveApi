86e28ae743fd4a116f71a3008d4fa5afb97c09c0
MohamedBerik <monsieur.atef@gmail.com>
2026-05-21 09:35:47 +0300

    fix

--- app/Policies/PatientRadiologyPolicy.php
+++ app/Policies/PatientRadiologyPolicy.php
@@ -1,52 +1,52 @@
 <?php
 
-namespace App\Policies;
+// namespace App\Policies;
 
-use App\Models\PatientRadiology;
-use App\Models\User;
-use App\Services\Tenant;
-use Illuminate\Auth\Access\HandlesAuthorization;
+// use App\Models\PatientRadiology;
+// use App\Models\User;
+// use App\Services\Tenant;
+// use Illuminate\Auth\Access\HandlesAuthorization;
 
-class PatientRadiologyPolicy
-{
-    use HandlesAuthorization;
+// class PatientRadiologyPolicy
+// {
+//     use HandlesAuthorization;
 
-    public function viewAny(User $user): bool
-    {
-        return $this->hasAccess($user);
-    }
+//     public function viewAny(User $user): bool
+//     {
+//         return $this->hasAccess($user);
+//     }
 
-    public function view(User $user, PatientRadiology $radiology): bool
-    {
-        return $this->hasAccess($user, $radiology->company_id);
-    }
+//     public function view(User $user, PatientRadiology $radiology): bool
+//     {
+//         return $this->hasAccess($user, $radiology->company_id);
+//     }
 
-    public function create(User $user): bool
-    {
-        return $this->hasAccess($user);
-    }
+//     public function create(User $user): bool
+//     {
+//         return $this->hasAccess($user);
+//     }
 
-    public function update(User $user, PatientRadiology $radiology): bool
-    {
-        return $this->hasAccess($user, $radiology->company_id);
-    }
+//     public function update(User $user, PatientRadiology $radiology): bool
+//     {
+//         return $this->hasAccess($user, $radiology->company_id);
+//     }
 
-    public function delete(User $user, PatientRadiology $radiology): bool
-    {
-        return $this->hasAccess($user, $radiology->company_id);
-    }
+//     public function delete(User $user, PatientRadiology $radiology): bool
+//     {
+//         return $this->hasAccess($user, $radiology->company_id);
+//     }
 
-    private function hasAccess(User $user, $companyId = null): bool
-    {
-        // Super Admin
-        if ($user->is_super_admin) {
-            if (Tenant::hasTenant()) {
-                return $companyId == Tenant::id();
-            }
-            return true;
-        }
+//     private function hasAccess(User $user, $companyId = null): bool
+//     {
+//         // Super Admin
+//         if ($user->is_super_admin) {
+//             if (Tenant::hasTenant()) {
+//                 return $companyId == Tenant::id();
+//             }
+//             return true;
+//         }
 
-        // Regular user
-        return $companyId == $user->company_id;
-    }
-}
+//         // Regular user
+//         return $companyId == $user->company_id;
+//     }
+// }
