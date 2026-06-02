1c8579c924c8b7b978d330d99619a0647efbd1ca
MohamedBerik <monsieur.atef@gmail.com>
2026-06-02 23:46:16 +0300

    fix

--- app/Events/AlertCreated.php
+++ app/Events/AlertCreated.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\Models\SystemAlert;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class AlertCreated implements ShouldBroadcast
+class AlertCreated implements ShouldBroadcastNow
 {
     use Dispatchable, SerializesModels;
 
