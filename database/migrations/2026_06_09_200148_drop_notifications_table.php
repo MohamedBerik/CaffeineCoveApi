79dc3a9a425ff0daa0c3218852a55ee0c7a329c2
MohamedBerik <monsieur.atef@gmail.com>
2026-06-09 20:11:30 +0300

    fix

---
+++ database/migrations/2026_06_09_200148_drop_notifications_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('notifications');
+    }
+
+    public function down()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+        });
+    }
+}
