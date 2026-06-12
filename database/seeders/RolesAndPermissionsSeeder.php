38eb1bc9409a40bf2c9e6a5c5d5ef57614f90ba7
MohamedBerik <monsieur.atef@gmail.com>
2026-06-12 20:01:22 +0300

    fix

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -155,6 +155,8 @@ class RolesAndPermissionsSeeder extends Seeder
             'dental_records.view',
             'inventory.view',
             'notifications.view',
+            'finance.view',
+
         ]);
     }
 }
